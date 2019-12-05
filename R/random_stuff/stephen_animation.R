11a5dfc347388be289ea69bbdb62f04614eb6fdb
Jordan-Krogmann <jordanckrogmann@gmail.com>
2019-12-05 07:27:04 -0500

    cleaned code

--- R/random_stuff/stephen_animation.R
+++ R/random_stuff/stephen_animation.R
@@ -8,7 +8,7 @@ library(tweenr)
 
 # ---------------------------------
 
-x <- seq(1,100, by = 1)
+x <- seq(1,100, by = .5)
 y <- x ^ 2
 
 df <- as_tibble(cbind(x,y))
@@ -18,24 +18,25 @@ text <- "Lobster Divide"
 
 kara_sun_gif <- 
   ggplot(data = df, aes(x = x, y = y)) +
-  geom_point(aes(colour = y),size = 2.5) +
+  geom_path(aes(colour = y),size = 2.5) +
   geom_vline(xintercept = 65, linetype = 5, size = 1.2) +
   geom_text(aes(x = 65, y = 7500, label = text, angle = 90), vjust = -1, size = 6) +
   scale_color_gradient(low = "dodgerblue4", high = "red") +
+  scale_y_continuous(labels = scales::comma) + 
   theme_minimal() +
   theme(
-    legend.position="none",
-    plot.title = element_text(face = "bold", size = 16),
-    axis.title = element_text(face = "bold", size = 14),
-    plot
+    legend.position = "none",
+    plot.title = element_text(face = "bold", size = 18, hjust = .5),
+    axis.title = element_text(face = "bold", size = 16),
+    axis.text = element_text(face = "bold", size = 14)
     ) + 
   labs(title = "Kara's Sun Exposure",
          y = "Amount of Sunburn",
          x = "Time") + 
 
-  transition_manual(x) +
+  transition_manual(x, cumulative = TRUE) +
   ease_aes('linear') 
 
 
-kara_sun_gif2 <- gganimate::animate(kara_sun_gif, end_pause = 10, height = 500, width =800)
+kara_sun_gif2 <- gganimate::animate(kara_sun_gif, end_pause = 10, height = 500, width = 800)
 gganimate::anim_save(kara_sun_gif2, filename = here::here("plots_gifs","kara_sun_gif.gif"))  
