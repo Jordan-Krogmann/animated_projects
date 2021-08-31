d4bd6288ffa610c6d06f0470244626e81373cea9
unknown <jordanckrogmann@gmail.com>
2021-08-30 21:51:37 -0400

    formatted

--- R/drunk_presentations/drunk_presentations.R
+++ R/drunk_presentations/drunk_presentations.R
@@ -22,19 +22,19 @@ drink_function <- function(
 
 # 
 humans_df <- tibble(
-    first_name = c("Jordan", "Tim", "Brittny", "William", "Julia", "Justin", "Jennilynn", "Tal", "Jason", "Lynn", "Dan", "Sarbina")
-  , last_name = c("Krogmann","Cerato", "Cerato", "Donovan", "Not Donovan", "Eastman", "Hagbi", "Hagbi", "Loy", "Chabot", "Debold", "Debold")
-  , body_weight = c(170, 210, 122, 250, 130, 180, 120, 445, 190, 140, 160, 130)
-  , pregnant_body_weight = c(NA, NA, 157, NA, NA, NA, NA, 470, NA, NA, NA, NA)
-  , gender = c("male","male","female","male","female","male","female","male","male","female","male","female")
+    first_name = c("Jordan", "Tim", "Brittny", "William", "Julia", "Justin", "Jennilynn", "Tal", "Jason", "Lynn", "Dan", "Sarbina"),
+    last_name = c("Krogmann","Cerato", "Cerato", "Donovan", "Not Donovan", "Eastman", "Hagbi", "Hagbi", "Loy", "Chabot", "Debold", "Debold"),
+    body_weight = c(170, 210, 122, 250, 130, 180, 120, 445, 190, 140, 160, 130), 
+    pregnant_body_weight = c(NA, NA, 157, NA, NA, NA, NA, 470, NA, NA, NA, NA), 
+    gender = c("male","male","female","male","female","male","female","male","male","female","male","female")
 )
 
 # create bac data frame
 bac_df <- tibble(
-    bac_level = seq(0 , 1 , by = .01)
-  , number_of_drinks = seq(0 , 10 , by = .1)
-  , body_weight = seq(0 , 500 , by = 5)
-  )
+    bac_level = seq(0 , 1 , by = .01), 
+    number_of_drinks = seq(0 , 10 , by = .1), 
+    body_weight = seq(0 , 500 , by = 5)
+)
 
 
 
