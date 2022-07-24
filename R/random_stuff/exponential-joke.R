be8cb382c7986b2bdf08ee226f798bbe97a1876e
Jordan-Krogmann <jordanckrogmann@gmail.com>
2022-07-24 17:27:13 -0400

    added new gif

--- R/random_stuff/exponential-joke.R
+++ R/random_stuff/exponential-joke.R
@@ -9,17 +9,17 @@ library(tweenr)
 # create data -----
 
 x <- seq(1, 100, by = .5)
-y <- x ^ 2
+y <- (x ^ 2) * -1
 df <- as_tibble(cbind(x,y))
 
-text <- "Lobster Divide"
+text <- "nOt gOoD bRaIn-GUd liNE"
 
-kara_sun_gif <- 
+g <- 
   ggplot(data = df, aes(x = x, y = y)) +
   geom_path(aes(colour = y), size = 2.5) +
   geom_vline(xintercept = 65, linetype = 5, size = 1.2) +
-  geom_text(aes(x = 65, y = 7500, label = text, angle = 90), vjust = -1, size = 6) +
-  scale_color_gradient(low = "dodgerblue4", high = "red") +
+  geom_text(aes(x = 65, y = -7500, label = text, angle = 90), vjust = -1, size = 6) +
+  scale_color_gradient(low = "red", high = "dodgerblue4") +
   scale_y_continuous(labels = scales::comma) + 
   theme_minimal() +
   theme(
@@ -29,14 +29,15 @@ kara_sun_gif <-
     axis.text = element_text(face = "bold", size = 14)
     ) + 
   labs(
-    title = "Kara's Sun Exposure",
-    y = "Amount of Sunburn",
+    title = "jOrDAn'S tHInkIEs",
+    subtitle = "a higher number is better... as you can see it's quite low",
+    y = "smaHHHtness",
     x = "Time"
   ) + 
   transition_manual(x, cumulative = TRUE) +
   ease_aes('linear') 
 
 
-kara_sun_gif2 <- gganimate::animate(kara_sun_gif, end_pause = 10, height = 500, width = 800)
+gif <- gganimate::animate(g, end_pause = 10, height = 500, width = 800)
 
-gganimate::anim_save(kara_sun_gif2, filename = here::here("plots_gifs", "kara_sun_gif.gif"))  
+gganimate::anim_save(gif, filename = here::here("images", "smart.gif"))  
